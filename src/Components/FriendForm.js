1757944cc111b443448d4a8e40297db23b73cc81
lowlyHero <e.nunn1009@gmail.com>
2019-05-29 14:34:08 -0400

    Setting up basic structure of components and importing/exporting them

---
+++ src/Components/FriendForm.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const FriendForm = () => {
+    return (
+    <form>
+        <input></input>
+    </form>
+    )}
+
+export default FriendForm
---
+++ src/Components/FriendsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const FriendsList = () => {
+    return (
+        <div>
+        <li>List</li>
+    </div>
+    )
+}
+
+export default FriendsList
